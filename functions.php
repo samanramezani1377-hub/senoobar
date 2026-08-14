e0432422e69922062835c5285574d5cfd70ff7db
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-15 02:14:54 +0330

    Implement cart assets loading in functions.php
    Added function to enqueue cart styles and scripts.

--- functions.php
+++ functions.php
@@ -10,3 +10,34 @@ function senoobar_init() {
     Senoobar_Theme::get_instance()->init();
 }
 add_action('after_setup_theme', 'senoobar_init');
+function senoobar_cart_assets() {
+
+    if ( is_cart() ) {
+
+        wp_enqueue_style(
+            'senoobar-cart',
+            get_template_directory_uri() . '/assets/css/cart.css',
+            array(),
+            '1.0.0'
+        );
+
+        wp_enqueue_script(
+            'senoobar-cart',
+            get_template_directory_uri() . '/assets/js/cart.js',
+            array('jquery'),
+            '1.0.0',
+            true
+        );
+
+        wp_localize_script(
+            'senoobar-cart',
+            'senoobar_cart',
+            array(
+                'ajax_url' => admin_url('admin-ajax.php'),
+                'nonce'    => wp_create_nonce('senoobar_cart_nonce')
+            )
+        );
+    }
+}
+
+add_action('wp_enqueue_scripts', 'senoobar_cart_assets');
