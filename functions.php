31985cfc1a693bd5dddd45a9fe53e21fde4f2438
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-20 19:14:30 +0330

    bulk-post-importer: add inline image import (any count/position), WEBP support; bump SENOOBAR_VERSION 2.15.0→2.16.0, CACHE_VERSION v3.9.0→v4.0.0

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-define('SENOOBAR_VERSION', '2.15.0');
+define('SENOOBAR_VERSION', '2.16.0');
 define('SENOOBAR_DIR', get_template_directory());
 define('SENOOBAR_URI', get_template_directory_uri());
 
